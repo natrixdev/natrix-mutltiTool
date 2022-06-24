9e6d7095beb90f47672112cf87ca63121715b74a
natrix_dev <discordlenko@gmail.com>
2022-06-24 19:08:26 +0200

    Create main.py

---
+++ PaidTool/main.py
@@ -0,0 +1,7 @@
+# Buy the tool first please. [All the informations in the README.md of this repo ! [https://github.com/natrixdev/natrix-mutltiTool]
+
+import githubviews; import os 
+for githubviews in github:
+  os.open('stats.txt', a) 
+  os.write($github.php/githubviews/views+1(user:natrixdev))
+  os.dir(close)
